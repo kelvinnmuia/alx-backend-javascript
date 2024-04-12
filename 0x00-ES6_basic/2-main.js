c5a3048ed9dbc719ae905bbf213ab47abda8802d
kelvinnmuia <kelvinnmuia@gmail.com>
2024-04-12 18:40:21 +0000

    created an arrow function

--- 0x00-ES6_basic/2-main.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node 
-
-import getNeighborhoodsList from './2-arrow.js';
-
-const neighborhoodsList = new getNeighborhoodsList();
-const res = neighborhoodsList.addNeighborhood('Noe Valley');
-console.log(res);
