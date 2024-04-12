10808bbe98a8468ea7e2cc4d037187541c79bd77
kelvinnmuia <kelvinnmuia@gmail.com>
2024-04-12 19:24:13 +0000

    created a function with loops

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-  let i = 0;
-  for (const element of array) {
-    array[i] = appendString + element;
-    i++;
+  const newArray = [];
+  for (const value of array) {
+    newArray.push(appendString + value);
   }
-  return array;
+
+  return newArray;
 }
