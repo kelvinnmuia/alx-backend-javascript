9c0d670aeecb1c04ae0235f3bd2217701879082f
kelvinnmuia <kelvinnmuia@gmail.com>
2024-04-15 12:59:00 +0000

    created a query API funtion

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Bezaleel Olakunori
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
