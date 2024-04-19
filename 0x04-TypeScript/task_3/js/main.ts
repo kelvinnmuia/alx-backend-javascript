c070e6f72c0072177b8c6a938f83a9c3eeb8d2c0
kelvinnmuia <kelvinnmuia@gmail.com>
2024-04-19 17:18:30 +0000

    created major and minor credit interfaces

--- 0x04-TypeScript/task_3/js/main.ts
+++ 0x04-TypeScript/task_3/js/main.ts
@@ -1,51 +1,12 @@
-export interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [index:string]: any;
-}
+/// <reference path="./crud.d.ts" />
+import { RowID, RowElement } from './interface';
+import * as CRUD from './crud';
 
-export interface Directors extends Teacher {
-  numberOfReports: number;
-}
-
-export interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-
-export function printTeacher(firstName: string, lastName: string): string {
-  return `${firstName[0]}. ${lastName}`;
-}
-
-export interface IStudentClassConstructor {
-  new (firstName: string, lastName: string): IStudentClass;
-}
-
-export interface IStudentClass {
-  workOnHomework(): string;
-  displayName(): string;
-}
-
-export class StudentClass implements IStudentClass {
-  private _firstName!: string;
-  private _lastName!: string;
-
-  constructor(firstName: string, lastName: string) {
-    this._firstName = firstName;
-    this._lastName = lastName;
-  }
-
-  workOnHomework() {
-    return 'Currently working';
-  }
-
-  displayName() {
-    return this._firstName;
-  }
-}
-
-export function createStudent(ctor: IStudentClassConstructor, firstName: string, lastName: string): IStudentClass {
-  return new ctor(firstName, lastName);
-}
+const row: RowElement = {
+  firstName: 'Guillaume',
+  lastName: 'Salva',
+};
+const newRowID: RowID = CRUD.insertRow(row);
+const updatedRow: RowElement = {...row, age: 23};
+CRUD.updateRow(newRowID, updatedRow);
+CRUD.deleteRow(newRowID);
